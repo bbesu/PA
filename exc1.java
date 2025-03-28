abb551cd1b12ad88896b3406c7c48b4d8143a66b
Besu <Sunartsyuy@gmail.com>
2025-03-28 20:45:17 -0300

    28/03

---
+++ exc1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1;
+
+public class exc1 {
+    public static void main(String[] args) {
+ 
+                         int num =10;
+                        
+                
+              if(num%2==0) {  
+                System.out.println(num + " é divisivel por 2.");
+              }else {
+                System.out.println(num + " não é divisivel por 2.");
+        }
+    }
+}
---
+++ exc2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2;
+
+/**
+ *
+ * @author Admin
+ */
+public class exc2 {
+
+    /**
+     * @param args the command line arguments
+     */
+       public static void main(String[] args) {
+                    
+        int num =7;
+        
+        if (num>0){
+        System.out.println(num + " é um numero posistivo.");
+        }else {
+        System.out.println(num + "é um numero negativo.");
+            
+        }
+    }
+}
