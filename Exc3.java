93623649443decdaf34fdfba943165a458689fab
Besu <Sunartsyuy@gmail.com>
2025-03-28 21:40:54 -0300

    Add files via upload
    28/03

---
+++ Exc3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exc3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exc3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int num = 150;
+    
+    if(num>100){
+        System.out.println(num + " é maior que 100");
+    }
+  }
+}
